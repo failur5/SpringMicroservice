fadafc4730c213444728b7a40e6d7c1d6929ade0
failur5 <conqueromanish@gmail.com>
2021-08-12 01:05:41 +0530

    First Commit

---
+++ cloud-config-server/src/test/java/com/devmanish/cloud/config/CloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmanish.cloud.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
