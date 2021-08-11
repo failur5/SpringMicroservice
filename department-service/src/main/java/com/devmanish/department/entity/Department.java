f740081500cf518a388072d24cc4772278e74a35
failur5 <conqueromanish@gmail.com>
2021-08-11 09:33:14 +0530

    Initial Commit

---
+++ department-service/src/main/java/com/devmanish/department/entity/Department.java
@@ -0,0 +1,37 @@
+package com.devmanish.department.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Department {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long departmentId;
+
+	private String departmentName;
+
+	private String departmentAddress;
+
+	private String departmentCode;
+
+	@Override
+	public String toString() {
+		return "Department [departmentId=" + departmentId + ", departmentName=" + departmentName
+				+ ", departmentAddress=" + departmentAddress + ", departmentCode=" + departmentCode + "]";
+	}
+	
+	
+}
